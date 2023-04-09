afa2d1066f31d9db4ac8ef6f2c9147cb8f61a87e
Galarius <galarius.is@gmail.com>
2023-04-09 21:42:32 +0300

    Fix release github actions

--- include/opencl.hpp
+++ include/opencl.hpp
@@ -2,9 +2,5 @@
 
 #define __CL_ENABLE_EXCEPTIONS
 #pragma warning(push, 0)
-#if defined(__APPLE__) || defined(__MACOSX) || defined(WIN32)
-    #include "opencl/cl.hpp"
-#else
-    #include <CL/cl.hpp>
-#endif
+#include "opencl/cl.hpp"
 #pragma warning(pop)
