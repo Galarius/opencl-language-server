3279b34949371f9b8e386c138c9b1140ff139bd0
Galarius <galarius.is@gmail.com>
2023-08-08 00:14:28 +0300

    Add 'diagnostics' subcommand

--- tests/mocks/diagnostics-mock.hpp
+++ tests/mocks/diagnostics-mock.hpp
@@ -14,9 +14,13 @@ class DiagnosticsMock : public ocls::IDiagnostics
 public:
     MOCK_METHOD(void, SetBuildOptions, (const nlohmann::json&), (override));
 
+    MOCK_METHOD(void, SetBuildOptions, (const std::string&), (override));
+
     MOCK_METHOD(void, SetMaxProblemsCount, (uint64_t), (override));
 
     MOCK_METHOD(void, SetOpenCLDevice, (uint32_t), (override));
 
-    MOCK_METHOD(nlohmann::json, Get, (const ocls::Source&), (override));
+    MOCK_METHOD(std::string, GetBuildLog, (const ocls::Source&), (override));
+    
+    MOCK_METHOD(nlohmann::json, GetDiagnostics, (const ocls::Source&), (override));
 };
